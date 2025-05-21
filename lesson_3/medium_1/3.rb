398762e46c76148afa30baeb722cb80cf0421f37
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-05-21 16:10:43 -0400

    Small progress on Med 1

--- lesson_3/medium_1/3.rb
+++ lesson_3/medium_1/3.rb
@@ -8,3 +8,31 @@ def factors(number)
   factors
 end
 
+# Problem with above:
+# will fail if input is 0 or negative
+# we don't want a begin / end until loop
+
+# Bonus question 1: the purposes of number % divisor == 0 is to check for the numbers that divide evenly into our number
+
+# Bonus question 2: the purpose of line 8 above is to return the array of divisors
+
+def factors2(number)
+  return 'All non-zero numbers' if number == 0
+  
+  if number < 0 
+    negative_flag = 1
+    number *= -1
+  end
+  divisor = number
+  factors = []
+  until divisor == 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors.map! { |x| x * -1 } if negative_flag == 1
+  factors
+
+end
+p factors2(0)
+p factors2(10)
+p factors2(-10)
